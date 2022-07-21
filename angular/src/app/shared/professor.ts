6a4c3f652ae3d5fe47f6557143a10bec579ecf1e
dimashaheen <dimanasser2001@gmail.com>
2022-07-21 15:55:27 +0200

    apiServices

---
+++ angular/src/app/shared/professor.ts
@@ -0,0 +1,8 @@
+export class Professor {
+    "_id": String;
+    "professor_username": String;
+    "professor_firstname": String;
+    "professor_lastname": String;
+    "professor_email": String;
+    "age": Number;
+ }
